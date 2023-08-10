c1471e488dbc66a68f4d2313d74d0c8f4482d4f5
albertocastrovill <josealberto.castro@udem.edu>
2023-08-09 22:19:48 -0600

    Hola Mundo - Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <Arduino.h>
 
-int arr[10] = {1, 2, 3, 4, 1, 1, 1, 1, 1, 1};
+int arr[10] = {5, 3, 8, 4, 1, 6, 2, 9, 7};
 
 void setup()
 {
+    // Iniciar la comunicación serie a 9600 baudios
+    Serial.begin(9600);
 }
 
 void loop()
 {
+    // Imprimir "Hola mundo" en la consola
+  Serial.println("Hola mundo");
+  delay(2000);                   // Esperar un segundo (2000 milisegundos)
 }
