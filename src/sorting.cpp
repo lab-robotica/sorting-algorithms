a706fc2038b972b152ef9436e3d1e928b44430d8
G00Z_G00Z <edygomezgg4@gmail.com>
2023-08-10 12:05:58 -0600

    Bubble sort

--- src/sorting.cpp
+++ src/sorting.cpp
@@ -1,4 +1,3 @@
-
 #include <Arduino.h>
 
 /**
@@ -9,4 +8,17 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+    for (size_t step = 0; step < size; step++)
+    {
+        for (int i = 0; i < size - step; i++)
+        {
+            // Comprare the numbers and swap them
+            if (array[i] > array[i + 1])
+            {
+                int temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+    }
 }
