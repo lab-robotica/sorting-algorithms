f93ac23b1732f35176ba512ab852ba51a2d7ef2f
G00Z_G00Z <edygomezgg4@gmail.com>
2023-08-10 12:20:28 -0600

    Bubble sort finished

--- src/main.cpp
+++ src/main.cpp
@@ -2,58 +2,76 @@
 #include "sorting.h"
 
 int arr[] = {5, 3, 8, 4, 1, 6, 2, 9, 7};
-int sizeOfArray;
+int sizeOfArray = 9;
+void displayArrays(int arrayData[], int sizeOfData);
+int findMax(int dataSet[]);
+int findSizeOfArray(int numOfBytes, int myVector[]);
 
 void setup()
 {
 	// Iniciar la comunicación serie a 9600 baudios
 	Serial.begin(9600);
-	bubble_sort(arr, 10);
-  sizeOfArray = findSizeOfArray(sizeof(arr), arr);
-  Serial.print("The Array is: ");
-  displayArrays(arr, sizeOfArray);
-  int largestNumber = findMax(arr);
-  Serial.print("The largest value: ");
-  Serial.println(largestNumber);
+
+	const int ARRAY_SIZE = findSizeOfArray(sizeof(arr), arr);
+	Serial.print("Size arreglo:");
+	Serial.println(ARRAY_SIZE);
+
+	// Arreglo desordenado
+	Serial.println("Arreglo desordenado:");
+	displayArrays(arr, ARRAY_SIZE);
+
+	// Ordenar el arreglo
+	Serial.println("Ordenando: ");
+	bubble_sort(arr, ARRAY_SIZE);
+
+	// Arreglo desordenado
+	Serial.println("Arreglo ordenado:");
+	displayArrays(arr, ARRAY_SIZE);
+
+	// Numero más grande
+	int largestNumber = findMax(arr);
+	Serial.print("The largest value: ");
+	Serial.println(largestNumber);
 }
 
 void loop()
 {
-	
 }
 
 int findSizeOfArray(int numOfBytes, int myVector[])
 {
-  int sizeOfVector = numOfBytes/sizeof(arr[0]);
-  return sizeOfVector;
+	int sizeOfVector = numOfBytes / sizeof(arr[0]);
+	return sizeOfVector;
 }
 
 int findMax(int dataSet[])
 {
-  static int i = 0;
-  static int max = -9999;
-  if (i < sizeOfArray)
-  {
-    if ( max < dataSet[i])
-    {
-      max = dataSet[i];
-    }
-    i++;
-    findMax(dataSet);
-  }
-  return max;
+	static int i = 0;
+	static int max = -9999;
+	if (i < sizeOfArray)
+	{
+		if (max < dataSet[i])
+		{
+			max = dataSet[i];
+		}
+		i++;
+		findMax(dataSet);
+	}
+	return max;
 }
 
 void displayArrays(int arrayData[], int sizeOfData)
 {
-  for ( int i = 0 ; i<sizeOfData; i++)
-  {
-    if (i<sizeOfData-1)
-    {
-      Serial.print(arrayData[i]);
-      Serial.println(", ");
-    }else{
-      Serial.println(arrayData[i]);
-    }
-  }
+	for (int i = 0; i < sizeOfData; i++)
+	{
+		if (i < sizeOfData - 1)
+		{
+			Serial.print(arrayData[i]);
+			Serial.println(", ");
+		}
+		else
+		{
+			Serial.println(arrayData[i]);
+		}
+	}
 }
--- src/sorting.cpp
+++ src/sorting.cpp
@@ -10,7 +10,7 @@ void bubble_sort(int *array, size_t size)
 {
     for (size_t step = 0; step < size; step++)
     {
-        for (int i = 0; i < size - step; i++)
+        for (int i = 0; i < size - step - 1; i++)
         {
             // Comprare the numbers and swap them
             if (array[i] > array[i + 1])
