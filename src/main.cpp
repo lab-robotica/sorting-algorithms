c3f20663d14538efc7d277f9f092456f08bb1c47
G00Z_G00Z <edygomezgg4@gmail.com>
2023-08-16 12:26:02 -0600

    Plotting correclty

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@ Alumnos:
 #include "utils.h"
 #include <Arduino.h>
 
+using time_unit_s = unsigned long;
 const int MAX_ARRAY_SIZE = 120;
 const int MIN_ARRAY_SIZE = 25;
 const int ARRAY_STEP = 5;
@@ -22,11 +23,24 @@ int ARRAY_SIZES[NUM_ARRAYS] = {};
 
 int arrays[NUM_ARRAYS][MAX_ARRAY_SIZE] = {};
 
+enum SortingAlgo
+{
+  BUBBLE_SORT = 0,
+  INSERTION_SORT = 1,
+  QUICK_SORT = 2
+};
+
+/**
+ * @brief Stores the time it takes to sort each array
+ *
+ */
+time_unit_s sortingTimes[NUM_ARRAYS][3] = {};
+
 /**
  * @brief Populates the variable with random data
  *
  */
-void randomizeArrays()
+void randomizeAllArrays()
 {
   for (int i = 0; i < NUM_ARRAYS; i++)
   {
@@ -34,30 +48,60 @@ void randomizeArrays()
   }
 }
 
+void printResults(int size, time_unit_s bubbleSortTime, time_unit_s insertionSortTime, time_unit_s quickSortTime)
+{
+  Serial.print("Size:");
+  Serial.print(size);
+  Serial.print(",");
+  Serial.print("BubbleSortTime:");
+  Serial.print(bubbleSortTime);
+  Serial.print(",");
+  Serial.print("InsertionSortTime:");
+  Serial.print(insertionSortTime);
+  Serial.print(",");
+  Serial.print("QuickSortTime:");
+  Serial.println(quickSortTime);
+}
+
 void setup()
 {
 
   Serial.begin(9600);
-  Serial.println("Initializing arrays");
   // Initialize array sizes variable
   for (int i = 0; i < NUM_ARRAYS; i++)
   {
     ARRAY_SIZES[i] = MIN_ARRAY_SIZE + i * ARRAY_STEP;
   }
 
-  randomizeArrays();
-  Serial.println("Done");
+  // Make the trials and save the results
+  time_unit_s start, end;
+  randomizeAllArrays();
+  for (int i = 0; i < NUM_ARRAYS; i++)
+  {
 
-  Serial.println("Random array example 1: ");
-  displayArrays(arrays[0], ARRAY_SIZES[0]);
+    start = micros();
+    bubble_sort(arrays[i], ARRAY_SIZES[i]);
+    end = micros();
+    sortingTimes[i][BUBBLE_SORT] = end - start;
 
-  Serial.println("Random sorting with quicksort 1: ");
-  myInsertionSort(arrays[0], ARRAY_SIZES[0]);
+    populateArrayWithRandomData(arrays[i], ARRAY_SIZES[i]);
 
-  Serial.println("Sorted array example 1: ");
-  displayArrays(arrays[0], ARRAY_SIZES[0]);
-  Serial.print("Is it sorted");
-  Serial.println(isSorted(arrays[0], ARRAY_SIZES[0]));
+    start = micros();
+    myInsertionSort(arrays[i], ARRAY_SIZES[i]);
+    end = micros();
+    sortingTimes[i][INSERTION_SORT] = end - start;
+
+    populateArrayWithRandomData(arrays[i], ARRAY_SIZES[i]);
+
+    start = micros();
+    quickSort(arrays[i], 0, ARRAY_SIZES[i] - 1);
+    end = micros();
+    sortingTimes[i][QUICK_SORT] = end - start;
+
+    printResults(ARRAY_SIZES[i], sortingTimes[i][BUBBLE_SORT], sortingTimes[i][INSERTION_SORT], sortingTimes[i][QUICK_SORT]);
+  }
 }
 
-void loop() {}
+void loop()
+{
+}
