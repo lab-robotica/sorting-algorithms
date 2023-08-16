7b34819e100a40d2970aae9d523026ed723fb8d5
G00Z_G00Z <edygomezgg4@gmail.com>
2023-08-16 11:14:56 -0600

    Setup complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,46 @@
 #include <Arduino.h>
 #include "sorting.h"
+#include "utils.h"
 
-int arr[] = {5, 3, 8, 4, 1, 6, 2, 9, 7};
-int sizeOfArray = 9;
-void displayArrays(int arrayData[], int sizeOfData);
-int findMax(int dataSet[]);
-int findSizeOfArray(int numOfBytes, int myVector[]);
+const int MAX_ARRAY_SIZE = 120;
+const int MIN_ARRAY_SIZE = 25;
+const int ARRAY_STEP = 5;
+const int NUM_ARRAYS = (MAX_ARRAY_SIZE - MIN_ARRAY_SIZE) / ARRAY_STEP + 1;
+
+int ARRAY_SIZES[NUM_ARRAYS] = {};
+
+int arrays[NUM_ARRAYS][MAX_ARRAY_SIZE] = {};
+
+/**
+ * @brief Populates the variable with random data
+ *
+ */
+void randomizeArrays()
+{
+	for (int i = 0; i < NUM_ARRAYS; i++)
+	{
+		populateArrayWithRandomData(arrays[i], ARRAY_SIZES[i]);
+	}
+}
 
 void setup()
 {
-	// Iniciar la comunicación serie a 9600 baudios
+
 	Serial.begin(9600);
+	Serial.println("Initializing arrays");
+	// Initialize array sizes variable
+	for (int i = 0; i < NUM_ARRAYS; i++)
+	{
+		ARRAY_SIZES[i] = MIN_ARRAY_SIZE + i * ARRAY_STEP;
+	}
 
-	const int ARRAY_SIZE = findSizeOfArray(sizeof(arr), arr);
-	Serial.print("Size arreglo:");
-	Serial.println(ARRAY_SIZE);
+	randomizeArrays();
+	Serial.println("Done");
 
-	// Arreglo desordenado
-	Serial.println("Arreglo desordenado:");
-	displayArrays(arr, ARRAY_SIZE);
-
-	// Ordenar el arreglo
-	Serial.println("Ordenando: ");
-	bubble_sort(arr, ARRAY_SIZE);
-
-	// Arreglo desordenado
-	Serial.println("Arreglo ordenado:");
-	displayArrays(arr, ARRAY_SIZE);
-
-	// Numero más grande
-	int largestNumber = findMax(arr);
-	Serial.print("The largest value: ");
-	Serial.println(largestNumber);
+	Serial.println("Random array example 1: ");
+	displayArrays(arrays[0], ARRAY_SIZES[0]);
 }
 
 void loop()
 {
-}
-
-int findSizeOfArray(int numOfBytes, int myVector[])
-{
-	int sizeOfVector = numOfBytes / sizeof(arr[0]);
-	return sizeOfVector;
-}
-
-int findMax(int dataSet[])
-{
-	static int i = 0;
-	static int max = -9999;
-	if (i < sizeOfArray)
-	{
-		if (max < dataSet[i])
-		{
-			max = dataSet[i];
-		}
-		i++;
-		findMax(dataSet);
-	}
-	return max;
-}
-
-void displayArrays(int arrayData[], int sizeOfData)
-{
-	for (int i = 0; i < sizeOfData; i++)
-	{
-		if (i < sizeOfData - 1)
-		{
-			Serial.print(arrayData[i]);
-			Serial.println(", ");
-		}
-		else
-		{
-			Serial.println(arrayData[i]);
-		}
-	}
 }
